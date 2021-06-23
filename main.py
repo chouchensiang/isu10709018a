16d363d48ec3f60ce6e1d92a7410b2ebaaffd15c
chouchensiang <86358556+chouchensiang@users.noreply.github.com>
2021-06-23 21:30:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-main=Flask(__name__)
-@main.route("/")
+app=Flask(__name__)
+@app.route("/")
 def index():
     return "10709018A"
-@main.route("/name")
+@app.route("/name")
 def who():
     return "jack"
 if __name__ == "__main__":
-    main.run()
+    app.run(host="127.0.0.1",port=8080,debug=False)
